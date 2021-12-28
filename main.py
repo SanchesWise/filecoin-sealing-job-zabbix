3b872a8ce9237a9969a28bf5d25f8678dff8caf6
ccsfarm <u11182s@gmail.com>
2021-12-29 02:04:55 +0300

    2

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-import os
-import time
+
 import fileinput
-from itertools import count
+
 
 sector_list = []
 # dict.fromkeys(['PID', 'WORKER', 'STAGE', 'STATE', 'TIME'])
@@ -14,11 +13,12 @@ freezePC2_count = 0
 workerPC1count = 2
 workerPC1current_count=0
 workerPC1summary_sectors =0
+dict_blanc = ['#PID', '#WORKER', '#STAGE', '#STATE', '#TIME']
 
 with open(file_name, mode='r', encoding='utf8') as file:
     # with fileinput.input() as file:
     for line in file:
-        sector_list.append(dict(zip(['#PID', '#WORKER', '#STAGE', '#STATE', '#TIME'], line[:-1].split(' '))))
+        sector_list.append(dict(zip(dict_blanc, line[:-1].split(' '))))
 
 print(f'{sector_list}')
 for sector in sector_list:
@@ -41,7 +41,7 @@ for sector in sector_list:
     if len(sector['#TIME']) > 8 and sector['#STAGE'] == 'PC2':
         print(sector['#TIME'], sector['#STAGE'], sector['#PID'])
         freezePC2_count += 1
-
+    massive +=
 for keys in workers_sectors_number:
     if workers_sectors_number[keys][1] > 0:
         workerPC1current_count += 1
@@ -55,5 +55,7 @@ print(f'abnormal_count = {abnormal_count}, freezePC2_count = {freezePC2_count}')
 print(f'{workers_sectors_number}')
 print(f'workerPC1summary_sectors {workerPC1summary_sectors} workerPC1current_count {10*workerPC1current_count}')
 massiv = str(workers_sectors_number)
-massiv =
+massiv = (f'[\{"#{WORKER.NAME}":{workers_sectors_number[1],}
 print(f'{massiv}')
+
+[{"{#UNIT.NAME}":"console-screen.service",
