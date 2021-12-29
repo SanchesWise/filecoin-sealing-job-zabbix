a07777acdf46c3a1b22cec7eef43c7596eafd867
ccsfarm <u11182s@gmail.com>
2021-12-29 11:37:03 +0300

    0.1 ready

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ workers = []
 abnormal_count = 0
 freezePC2_count = 0
 workerPC1count = 2
-workerPC1current_count=0
-workerPC1summary_sectors =0
+workerPC1current_count = 0
+workerPC1summary_sectors = 0
 dict_blanc = ['#PID', '#WORKER', '#STAGE', '#STATE', '#TIME']
 worker_phase_list = []
 
-#with open(file_name, mode='r', encoding='utf8') as file:
+# with open(file_name, mode='r', encoding='utf8') as file:
 with fileinput.input() as file:
     for line in file:
         sector_list.append(dict(zip(dict_blanc, line[:-1].split(' '))))
 
-#print(f'{sector_list}')
+# print(f'{sector_list}')
 for sector in sector_list:
     workers.append(sector['#WORKER'])
 workerslist = list(set(workers))
@@ -30,7 +30,7 @@ for worker in workerslist:
     worker_phase_list.append(f"\x7B#{worker}_TOTAL\x7D")
     worker_phase_list.append(f'\x7B#{worker}_PC1\x7D')
     worker_phase_list.append(f'\x7B#{worker}_PC2\x7D')
-#print(f"{worker_phase_list}")
+# print(f"{worker_phase_list}")
 workers_sectors_number = {worker: 0 for worker in worker_phase_list}
 
 for sector in sector_list:
@@ -44,7 +44,7 @@ for sector in sector_list:
     if sector['#STAGE'] == 'abnormal':
         abnormal_count += 1
     if len(sector['#TIME']) > 8 and sector['#STAGE'] == 'PC2':
-        #print(sector['#TIME'], sector['#STAGE'], sector['#PID'])
+        # print(sector['#TIME'], sector['#STAGE'], sector['#PID'])
         freezePC2_count += 1
 
 for keys in workers_sectors_number:
@@ -52,7 +52,7 @@ for keys in workers_sectors_number:
         workerPC1current_count += 1
         workerPC1summary_sectors += workers_sectors_number[keys]
 
-if workerPC1summary_sectors < 10*workerPC1current_count:
+if workerPC1summary_sectors < 10 * workerPC1current_count:
     pass
 if workerPC1current_count < workerPC1count:
     pass
@@ -61,6 +61,6 @@ if workerPC1current_count < workerPC1count:
 # print(f'workerPC1summary_sectors {workerPC1summary_sectors} workerPC1current_count {10*workerPC1current_count}')
 
 
-#[{"{#UNIT.NAME}":"console-screen.service",
-json_object = json.dumps(workers_sectors_number, indent = 1)
+# [{"{#UNIT.NAME}":"console-screen.service",
+json_object = json.dumps(workers_sectors_number, indent=1)
 print(json_object)
