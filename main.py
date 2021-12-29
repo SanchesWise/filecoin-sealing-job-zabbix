4642a26fd921ec8e5ed7d0a7bedc32ec412d53a0
ccsfarm <u11182s@gmail.com>
2021-12-29 11:36:06 +0300

    0.1 ready

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import fileinput
-
+import json
 
 sector_list = []
 # dict.fromkeys(['PID', 'WORKER', 'STAGE', 'STATE', 'TIME'])
@@ -14,48 +14,53 @@ workerPC1count = 2
 workerPC1current_count=0
 workerPC1summary_sectors =0
 dict_blanc = ['#PID', '#WORKER', '#STAGE', '#STATE', '#TIME']
+worker_phase_list = []
 
-with open(file_name, mode='r', encoding='utf8') as file:
-    # with fileinput.input() as file:
+#with open(file_name, mode='r', encoding='utf8') as file:
+with fileinput.input() as file:
     for line in file:
         sector_list.append(dict(zip(dict_blanc, line[:-1].split(' '))))
 
-print(f'{sector_list}')
+#print(f'{sector_list}')
 for sector in sector_list:
     workers.append(sector['#WORKER'])
 workerslist = list(set(workers))
 workerslist.sort()
-workers_sectors_number = {worker: [0, 0] for worker in workerslist}
-print(f'{workerslist}')
-print(f'{workers_sectors_number}')
+for worker in workerslist:
+    worker_phase_list.append(f"\x7B#{worker}_TOTAL\x7D")
+    worker_phase_list.append(f'\x7B#{worker}_PC1\x7D')
+    worker_phase_list.append(f'\x7B#{worker}_PC2\x7D')
+#print(f"{worker_phase_list}")
+workers_sectors_number = {worker: 0 for worker in worker_phase_list}
 
 for sector in sector_list:
     for worker in workerslist:
         if sector['#WORKER'] == worker:
-            workers_sectors_number[worker][0] += 1
+            workers_sectors_number[f"\x7B#{worker}_TOTAL\x7D"] += 1
             if sector['#STAGE'] == "PC1":
-                workers_sectors_number[worker][1] += 1
-
+                workers_sectors_number[f"\x7B#{worker}_PC1\x7D"] += 1
+            if sector['#STAGE'] == "PC2":
+                workers_sectors_number[f"\x7B#{worker}_PC2\x7D"] += 1
     if sector['#STAGE'] == 'abnormal':
         abnormal_count += 1
     if len(sector['#TIME']) > 8 and sector['#STAGE'] == 'PC2':
-        print(sector['#TIME'], sector['#STAGE'], sector['#PID'])
+        #print(sector['#TIME'], sector['#STAGE'], sector['#PID'])
         freezePC2_count += 1
-    massive +=
+
 for keys in workers_sectors_number:
-    if workers_sectors_number[keys][1] > 0:
+    if workers_sectors_number[keys] > 0 and "PC1" in keys:
         workerPC1current_count += 1
-        workerPC1summary_sectors += workers_sectors_number[keys][1]
+        workerPC1summary_sectors += workers_sectors_number[keys]
 
 if workerPC1summary_sectors < 10*workerPC1current_count:
     pass
 if workerPC1current_count < workerPC1count:
     pass
-print(f'abnormal_count = {abnormal_count}, freezePC2_count = {freezePC2_count}')
-print(f'{workers_sectors_number}')
-print(f'workerPC1summary_sectors {workerPC1summary_sectors} workerPC1current_count {10*workerPC1current_count}')
-massiv = str(workers_sectors_number)
-massiv = (f'[\{"#{WORKER.NAME}":{workers_sectors_number[1],}
-print(f'{massiv}')
+# print(f'abnormal_count = {abnormal_count}, freezePC2_count = {freezePC2_count}')
+# print(f'{workers_sectors_number}')
+# print(f'workerPC1summary_sectors {workerPC1summary_sectors} workerPC1current_count {10*workerPC1current_count}')
 
-[{"{#UNIT.NAME}":"console-screen.service",
+
+#[{"{#UNIT.NAME}":"console-screen.service",
+json_object = json.dumps(workers_sectors_number, indent = 1)
+print(json_object)
