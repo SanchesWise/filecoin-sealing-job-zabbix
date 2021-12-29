78a0342c4fc2c41e5bf5add19daae383bdf88e7d
ccsfarm <u11182s@gmail.com>
2021-12-29 14:04:41 +0300

    0.2 ready

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ workerPC1current_count = 0
 workerPC1summary_sectors = 0
 dict_blanc = ['#PID', '#WORKER', '#STAGE', '#STATE', '#TIME']
 worker_phase_list = []
+json_ready = []
 
-# with open(file_name, mode='r', encoding='utf8') as file:
-with fileinput.input() as file:
+with open(file_name, mode='r', encoding='utf8') as file:
+#with fileinput.input() as file:
     for line in file:
         sector_list.append(dict(zip(dict_blanc, line[:-1].split(' '))))
 
@@ -27,30 +28,30 @@ for sector in sector_list:
 workerslist = list(set(workers))
 workerslist.sort()
 for worker in workerslist:
-    worker_phase_list.append(f"\x7B#{worker}_TOTAL\x7D")
-    worker_phase_list.append(f'\x7B#{worker}_PC1\x7D')
-    worker_phase_list.append(f'\x7B#{worker}_PC2\x7D')
+    worker_phase_list.append(f'{worker}_TOTAL')
+    worker_phase_list.append(f'{worker}_PC1')
+    worker_phase_list.append(f'{worker}_PC2')
 # print(f"{worker_phase_list}")
 workers_sectors_number = {worker: 0 for worker in worker_phase_list}
 
 for sector in sector_list:
     for worker in workerslist:
         if sector['#WORKER'] == worker:
-            workers_sectors_number[f"\x7B#{worker}_TOTAL\x7D"] += 1
+            workers_sectors_number[f"{worker}_TOTAL"] += 1
             if sector['#STAGE'] == "PC1":
-                workers_sectors_number[f"\x7B#{worker}_PC1\x7D"] += 1
+                workers_sectors_number[f"{worker}_PC1"] += 1
             if sector['#STAGE'] == "PC2":
-                workers_sectors_number[f"\x7B#{worker}_PC2\x7D"] += 1
+                workers_sectors_number[f"{worker}_PC2"] += 1
     if sector['#STAGE'] == 'abnormal':
         abnormal_count += 1
     if len(sector['#TIME']) > 8 and sector['#STAGE'] == 'PC2':
         # print(sector['#TIME'], sector['#STAGE'], sector['#PID'])
         freezePC2_count += 1
 
-for keys in workers_sectors_number:
-    if workers_sectors_number[keys] > 0 and "PC1" in keys:
+for key in workers_sectors_number:
+    if workers_sectors_number[key] > 0 and "PC1" in key:
         workerPC1current_count += 1
-        workerPC1summary_sectors += workers_sectors_number[keys]
+        workerPC1summary_sectors += workers_sectors_number[key]
 
 if workerPC1summary_sectors < 10 * workerPC1current_count:
     pass
@@ -62,5 +63,11 @@ if workerPC1current_count < workerPC1count:
 
 
 # [{"{#UNIT.NAME}":"console-screen.service",
-json_object = json.dumps(workers_sectors_number, indent=1)
+for keys in workers_sectors_number:
+    json_ready.append({'\x7B#NAME\x7D': keys, '\x7B#VALUE\x7D': workers_sectors_number[keys]})
+
+#print(json_ready)
+json_object = json.dumps(json_ready, indent=1)
 print(json_object)
+
+#[f"\x7B#{worker}_TOTAL\x7D"
