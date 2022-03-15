ade8a97042a00d4edd8f207af0f9bc943ceb1442
ccsfarm <u11182s@gmail.com>
2022-03-15 12:25:53 +0300

    0.4

--- main.py
+++ main.py
@@ -55,16 +55,17 @@ for key in workers_sectors_number:
     if workers_sectors_number[key] > 0 and "PC1" in key:
         workerPC1current_count += 1
         workerPC1summary_sectors += workers_sectors_number[key]
-
+#os.umask(0)
+with open("/var/log/filecoin-zabbix/pledge.log", mode='a', encoding='utf8') as file:
+    file.write(f'{datetime.today()}  Summary sectors PC1 : {workerPC1summary_sectors}\n ')
 if workerPC1summary_sectors < 20:
     with open("pledge.log", mode='a', encoding='utf8') as file:
-        file.write(f'{datetime.today()}  Summary sectors PC1 : {workerPC1summary_sectors}\n ')
-        os.system("/usr/local/bin/lotus-miner sectors pledge >> /home/ccsfarm/filecoin-sealing-job-zabbix/pledge.log 2>&1")
+        #file.write(f'{datetime.today()}  Summary sectors PC1 : {workerPC1summary_sectors}\n ')
+        os.system("/usr/local/bin/lotus-miner sectors pledge >> /var/log/filecoin-zabbix/pledge.log 2>&1")
         #status = subprocess.check_output(["/usr/local/bin/lotus-miner", "sectors", "pledge"], stderr=subprocess.STDOUT)
         #file.write(f'action : {status.decode("utf-8")}\n')
 
-with open("pledge.log", mode='a', encoding='utf8') as file:
-    file.write(f'{datetime.today()}  Summary sectors PC1 : {workerPC1summary_sectors}\n ')
+
 
 if workerPC1current_count < workerPC1count:
     pass
